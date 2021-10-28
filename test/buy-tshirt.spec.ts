e668fd3f811e62a7e0d108098080a88ad75e3d68
JuanCiroLo <juanciro35@gmail.com>
2021-10-27 20:37:47 -0500

    Improving locators

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -11,37 +11,47 @@ import {
   SummaryStepPage
 } from '../src/page'
 
+describe('Open the page', () => {
+  it('then the shop should be opened', async () => {
+    await browser.get('http://automationpractice.com/')
+  })
+})
+
 describe('Buy a t-shirt', () => {
   const menuContentPage: MenuContentPage = new MenuContentPage()
   const addressStepPage: AddressStepPage = new AddressStepPage()
   const bankPaymentPage: BankPaymentPage = new BankPaymentPage()
   const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage()
+  it('then should display a login form', async () => {
+    await menuContentPage.goToTShirtMenu()
+    await addressStepPage.addToCart()
+    await bankPaymentPage.proceedToCheckout()
+    await orderSummaryPage.proceedToCheckout()
+  })
+})
+
+describe('Login to aplication', () => {
   const paymentStepPage: PaymentStepPage = new PaymentStepPage()
+  it('then should be loged into the app', async () => {
+    await paymentStepPage.fillLoginForm()
+  })
+})
+
+describe('Select direction tu deliver', () => {
   const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage()
+  it('then should be proceed with the buy', async () => {
+    await productAddedModalPage.proceedToCheckout()
+  })
+})
+
+describe('Pay the bill', () => {
   const productListPage: ProductListPage = new ProductListPage()
   const signInStepPage: SignInStepPage = new SignInStepPage()
   const summaryStepPage: SummaryStepPage = new SummaryStepPage()
-
   it('then should be bought a t-shirt', async () => {
-    await browser.get('http://automationpractice.com/')
-    await menuContentPage.goToTShirtMenu()
-    // await browser.sleep(3000);
-    await addressStepPage.addToCart()
-    // await browser.sleep(3000);
-    await bankPaymentPage.proceedToCheckout()
-    // await browser.sleep(3000);
-    await orderSummaryPage.proceedToCheckout()
-    // await browser.sleep(3000);
-    await paymentStepPage.fillLoginForm()
-    // await browser.sleep(3000);
-    await productAddedModalPage.proceedToCheckout()
-    // await browser.sleep(3000);
     await productListPage.proceedToCheckout()
-    // await browser.sleep(3000);
     await signInStepPage.PayByBankWire()
-    // await browser.sleep(3000);
     await summaryStepPage.ConfirmMyOrder()
-    // await browser.sleep(3000);
     await expect($('#center_column > div > p > strong').getText()).toBe(
       'Your order on My Store is complete.'
     )
